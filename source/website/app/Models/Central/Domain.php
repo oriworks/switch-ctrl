4e8787671fe120075ab5960ade4d307a7ed83432
Joel Viegas <joeloliveira@oriworks.com>
2024-02-22 16:40:43 +0000

    feat: Install tenancy for Laravel package

---
+++ source/website/app/Models/Central/Domain.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Central;
+
+use Stancl\Tenancy\Database\Models\Domain as BaseDomain;
+
+class Domain extends BaseDomain
+{
+    //
+}
--- source/website/database/seeders/DatabaseSeeder.php
+++ source/website/database/seeders/DatabaseSeeder.php
@@ -14,9 +14,22 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Joel Oliveira',
+            'email' => 'joeloliveira@oriworks.com',
+        ]);
+
+        $tenant = \App\Models\Central\Tenant::create([
+            'id' => 'padel',
+            // 'tenancy_db_connection' => 'padel',
+            // 'tenancy_db_driver' => 'mysql',
+            // 'tenancy_db_host' => '172.18.0.1',
+            // 'tenancy_db_port' => '33061',
+            // 'tenancy_db_username' => 'root',
+            // 'tenancy_db_password' => 'root',
+        ]);
+        $tenant->domains()->create([
+            'domain' => 'padel.switch-ctrl.local',
+        ]);
     }
 }
